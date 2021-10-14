8c28d02bc928c1e54ed61f5d64f44ef1252c1665
JananiVijayakumar <vsvprema@gmail.com>
2021-10-14 08:33:12 +0530

    emp wage

---
+++ Uc_six.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Uc_six{
+public void calculate_wage(){
+int count=0;
+int no_of_days=30;
+for (int i=0; i<=no_of_days; i++){
+Random r3=new Random();
+int calculate_wage=r3.nextInt(2);
+if(calculate_wage==1){
+count++;
+}
+System.out.println("no of days :"+count);
+int total_working_hrs=count*8;
+if(count<20 || total_working_hrs<100){
+int Salary_per_month=count*500;
+System.out.println("no of days employee coming to work :"+count);
+System.out.println("Salary per month depending on attendance:"+Salary_per_month);
+}
+else{
+System.out.println("Employee is not attend the work");
+}
+}
+}
+public static void main(String[] args)
+{
+Uc_six uc6=new Uc_six();
+uc6.calculate_wage();
+}
+}
